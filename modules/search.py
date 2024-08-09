7b8372dad7fa0e3c3fe960e7ad4f8423f9c7e00c
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-08-10 02:02:45 +0300

    Update search.py

--- modules/search.py
+++ modules/search.py
@@ -1,13 +1,33 @@
 import hashlib
+import logging
 from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 from duckduckgo_search import AsyncDDGS
 
+logging.basicConfig(level=logging.INFO)
 
 async def inline_search(inline_query: InlineQuery):
     query_text = inline_query.query.lower()
-    _, search_query = query_text.split(maxsplit=1)
+    logging.info(f"Received query: {query_text}")
+
+    if ' ' not in query_text:
+        logging.warning("No search query provided.")
+        await inline_query.answer([], cache_time=0)
+        return
+
+    try:
+        _, search_query = query_text.split(maxsplit=1)
+    except ValueError:
+        logging.warning("Failed to split query. No search query provided.")
+        await inline_query.answer([], cache_time=0)
+        return
+
     async with AsyncDDGS() as ddgs:
-        results = ddgs.text(search_query, max_results=15)
+        try:
+            results = await ddgs.text(search_query, max_results=15)
+        except Exception as e:
+            logging.error(f"Error while searching: {e}")
+            await inline_query.answer([], cache_time=0)
+            return
 
     articles = []
     for result in results:
@@ -22,8 +42,7 @@ async def inline_search(inline_query: InlineQuery):
         )
         articles.append(item)
 
-    await inline_query.answer(articles)
-
+    await inline_query.answer(articles, cache_time=0)
 
 def setup_tools_search(dp):
     pass
