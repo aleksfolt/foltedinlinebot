af8b9da2dac004fe37e869eef1e6041966c33ad7
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-08-09 23:58:50 +0300

    Update qr.py

--- modules/qr.py
+++ modules/qr.py
@@ -1,33 +1,29 @@
-import asyncio 
-from aiogram import types 
-from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-import requests
+import asyncio
+from aiogram import types
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 import hashlib
 
 async def inline_qr(inline_query: InlineQuery):
-     _, query_text = query_text.split(maxsplit=1)
-    
-    base_url= "https://api.qrserver.com/v1/create-qr-code/"
-    params= {
+    query_text = inline_query.query
+    base_url = "https://api.qrserver.com/v1/create-qr-code/"
+    params = {
         "size": "300x300",
         "data": query_text
     }
-    qr_code_url=f"{base_url}?size={params['size']}&data={params['data']}"
+    qr_code_url = f"{base_url}?size={params['size']}&data={params['data']}"
     
-    input_content = InputTextMessageContent(message_text=f"Your Qr: {qr_code_url}")
+    input_content = InputTextMessageContent(message_text=f"Your QR Code: {qr_code_url}")
     
-    result_id= hashlib.md5(qr_code_url.encode()).hexdigest()
+    result_id = hashlib.md5(qr_code_url.encode()).hexdigest()
     
-    item=InlineQueryResultArticle(
+    item = InlineQueryResultArticle(
         id=result_id,
         type="article",
-        title="QrCode Generator",
+        title="QR Code Generator",
         input_message_content=input_content
     )
     
     await inline_query.answer([item])
-    
     
 def setup_tools_qr(dp):
     dp.inline_query(inline_qr)
