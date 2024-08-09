14cda186c16841c3d54d107910b080d2e0a1e959
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-08-09 23:42:40 +0300

    Update qr.py

---
+++ modules/qr.py
@@ -0,0 +1,33 @@
+import asyncio 
+from aiogram import types 
+from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+import requests
+
+
+async def inline_qr(inline_query: InlineQuery, bot):
+    query_text= inline_query.query.lower()
+    
+    base_url= "https://api.qrserver.com/v1/create-qr-code/"
+    params= {
+        "size": "300x300",
+        "data": query_text
+    }
+    qr_code_url=f"{base_url}?size={params['size']}&data={params['data']}"
+    
+    input_content = InputTextMessageContent(message_text=f"Your Qr: {qr_code_url}")
+    
+    result_id= hashlib.md5(city.encode()).hexdigest()
+    
+    item=InlineQueryResultArticle(
+        id=result_id,
+        type="article",
+        title="QrCode Generator",
+        input_message_content=input_content
+    )
+    
+    await inline_query.answer([item])
+    
+    
+def setup_tools_qr(dp):
+    dp.inline_query(inline_qr)
