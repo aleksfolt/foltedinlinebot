51e3b0f137988df70c8cbebfa0ec3ee131dc0ac4
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-08-18 14:30:55 +0200

    Update wiki.py

--- modules/wiki.py
+++ modules/wiki.py
@@ -1,17 +1,27 @@
-import hashlib
+import logging
 from aiogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessageContent
 import wikipediaapi
+import hashlib
+
+# Настройка логгирования
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 wiki_wiki = wikipediaapi.Wikipedia(
     language='en',
     extract_format=wikipediaapi.ExtractFormat.WIKI,
     user_agent='foltuserbot/1.0'
-    )
-
+)
 
 async def inline_wiki(inline_query: InlineQuery):
     query_text = inline_query.query.lower()
-    _, search_query = query_text.split(maxsplit=1)
+    try:
+        _, search_query = query_text.split(maxsplit=1)
+    except ValueError as e:
+        logger.error("Ошибка разбиения запроса: %s", e)
+        return  # Останавливаем обработку, если запрос неправильный
+
+    logger.info("Поиск в Википедии по запросу: %s", search_query)
     page = wiki_wiki.page(search_query)
 
     articles = []
@@ -20,16 +30,15 @@ async def inline_wiki(inline_query: InlineQuery):
         result_id = hashlib.md5(page.title.encode()).hexdigest()
         item = InlineQueryResultArticle(
             id=result_id,
-            type="article",
             title=page.title,
             input_message_content=InputTextMessageContent(
                 message_text=f"{page.title}\n\n{summary}\n\nRead more: {page.fullurl}"
             )
         )
         articles.append(item)
+        logger.info("Страница найдена: %s", page.title)
+    else:
+        logger.info("Страница не найдена: %s", search_query)
 
     await inline_query.answer(articles)
-
-
-def setup_tools_wiki(dp):
-    dp.inline_query(inline_wiki)
+    logger.info("Ответ отправлен пользователю")
