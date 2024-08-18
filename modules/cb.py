dbd3def02867c499f2114e4fa06470d9fd4486ba
aleksfolt <122365476+aleksfolt@users.noreply.github.com>
2024-08-18 22:21:06 +0200

    Update cb.py

--- modules/cb.py
+++ modules/cb.py
@@ -1,7 +1,8 @@
 from aiogram import types
-from aiogram.types import InputFile, InputMediaPhoto, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineQueryResultPhoto, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton
 
-async def send_photo_with_caption_and_button(message: types.Message, bot):
+
+async def inline_photo_with_caption_and_button(inline_query, bot):
     photo_url = "https://tinypic.host/images/2024/08/18/IMG_0546.jpeg"
     caption = "🦋 [Чек](https://t.me/CryptoBotRU/23) на *10 USDT ($10)*."
     
@@ -9,13 +10,19 @@ async def send_photo_with_caption_and_button(message: types.Message, bot):
     button = InlineKeyboardButton(text="Получить 10 USDT", url="https://t.me/CryptoBotRU/23")
     keyboard.add(button)
 
-    await message.reply_photo(
-        photo=photo_url,
+    result_id = inline_query.id
+    input_content = InputTextMessageContent(message_text=caption, parse_mode="Markdown")
+
+    item = InlineQueryResultPhoto(
+        id=result_id,
+        photo_url=photo_url,
+        thumb_url=photo_url,
         caption=caption,
         reply_markup=keyboard,
-        parse_mode="Markdown",
-        cache_time=0
+        input_message_content=input_content
     )
 
+    await inline_query.answer([item], cache_time=0)
+
 def setup_tools_kb(dp):
-    dp.message_handler(commands=['kb'])(send_photo_with_caption_and_button)
+    dp.inline_query_handler(inline_photo_with_caption_and_button)
